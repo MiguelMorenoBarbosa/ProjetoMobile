ce87e0225648f8c38797273879c4fbe159931497
MiguelMorenoBarbosa <miguelmorenobarbosa7@gmail.com>
2023-05-15 10:26:04 -0300

    Deu certo

--- src/navigations/drawer.navigation.tsx
+++ src/navigations/drawer.navigation.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { DrawerNavigationProp, createDrawerNavigator } from '@react-navigation/drawer';
-import { ScreenPerfil } from "../screens"
+import { ScreenCamera, ScreenPerfil } from "../screens"
 import { colors } from '../styles/colors';
+import { Ionicons, AntDesign } from '@expo/vector-icons';
 
 type DrawerParamList = {
   Perfil: undefined
+  Camera: undefined
 }
 type DrawerScreenNavigationProp = DrawerNavigationProp<DrawerParamList, 'Perfil'>
 export type DrawerTypes = {
@@ -19,7 +21,20 @@ export function DrawerNavigation() {
       },
       drawerActiveTintColor: colors.white
     }}>
-      <Drawer.Screen name="Perfil" component={ScreenPerfil} />
-    </Drawer.Navigator>
+      <Drawer.Screen name="Perfil" component={ScreenPerfil} 
+        options={{
+          drawerIcon: () => (
+            <Ionicons name='person' size={24} color={colors.white} />
+          )
+        }}
+      />
+      <Drawer.Screen name="Camera" component={ScreenCamera} 
+      options={{
+        drawerIcon: () => (
+          <Ionicons name='camera' size={24} color={colors.white} />
+        )
+      }}
+    />
+    </Drawer.Navigator >
   );
 }
