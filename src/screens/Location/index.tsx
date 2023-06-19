d75d0a5c67c888d442982b3d1cf42ac465c037e1
MiguelMorenoBarbosa <miguelmorenobarbosa7@gmail.com>
2023-06-19 10:38:14 -0300

    Subir pro git

--- src/screens/Location/index.tsx
+++ src/screens/Location/index.tsx
@@ -1,16 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Text, View } from 'react-native';
 import * as Location from 'expo-location';
-import MapView, { Region, Marker } from "react-native-maps";
+import MapView, { Region, Marker, Polyline } from "react-native-maps";
 import { styles } from './styles';
+import { colors } from '../../styles/colors';
+import { API_GOOGLE } from '@env';
+import { GooglePlaceData, GooglePlaceDetail } from 'react-native-google-places-autocomplete';
+import {  } from 'react-native-maps-directions'
+
+type ICoords = {
+  latitude: number
+  longitude: number
+}
 
 export function LocationScreen() {
   const [location, setLocation] = useState<Location.LocationObject | null>(null);
   const [errorMsg, setErrorMsg] = useState<string | null>(null);
   const [region, setRegion] = useState<Region>()
   const [marker, setMarker] = useState<Region[]>()
+  const [coords, setCoords] = useState<ICoords[]>([])
+  const [destination, setDestination] = useState<Region | null>(null)
+  const mapRef = useRef<MapView>(null)
 
   useEffect(() => {
+    let subscription: Location.LocationSubscription
     (async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
@@ -25,30 +38,67 @@ export function LocationScreen() {
         latitudeDelta: 0.009,
         longitudeDelta: 0.009
     })
-    setMarker([
-        {
-            latitude: location.coords.latitude,
-            longitude: location.coords.longitude,
-            latitudeDelta: 0.009,
-            longitudeDelta: 0.009
-        }
-    ])
+    setMarker([{
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.009,
+          longitudeDelta: 0.009
+        }])
+      subscription = await Location.watchPositionAsync({
+        accuracy: Location.LocationAccuracy.High,
+        timeInterval: 1000,
+        distanceInterval: 1
+      }, (location) => {
+        setCoords((prevState) => [...prevState, location.coords])
+      });
     })();
+
+    return () => {
+      if (subscription) {
+        subscription.remove()
+      }
+    }
   }, []);
+
+  async function handleDestination(data: GooglePlaceData, details: GooglePlaceDetail | null) {
+    if (details) {
+        setDestination({
+            latitude: details?.geometry.location.lat, 
+            longitude: details?.geometry.location.lng,
+            latitudeDelta: 0.004,
+            longitudeDelta: 0.004
+        })
+        if (marker) {
+            setMarker([...marker, {
+                latitude: details?.geometry.location.lat, 
+                longitude: details?.geometry.location.lng, 
+                latitudeDelta: 0.004, 
+                longitudeDelta: 0.004
+            }])
+        }
+    }
+}
 
   let text = 'Waiting..';
   if (errorMsg) {
     text = errorMsg;
   } else if (location) {
     text = JSON.stringify(location);
+  } else if (destination) {
+    text = JSON.stringify(destination);
   }
   return (
     <View style={styles.map}>
         {region? (
-        <MapView region={region} style={styles.map}>
+        <MapView ref={mapRef} region={region} style={styles.map} showsUserLocation={true}>
             {marker && marker.map((i) => (
                 <Marker key={i.latitude} coordinate={i} />
             ))}
+            {coords && <Polyline
+            coordinates={coords}
+            strokeColor={colors.black}
+            strokeWidth={7}
+            />}
         </MapView>
         ) : (
             <Text style={styles.paragraph}>{text}</Text>
