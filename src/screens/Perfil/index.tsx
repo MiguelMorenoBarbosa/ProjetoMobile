888f8f73ef4365e66f1d49e9682fb8518d304ec5
MiguelMorenoBarbosa <miguelmorenobarbosa7@gmail.com>
2023-09-12 14:37:03 -0300

    Subir pro git

--- src/screens/Perfil/index.tsx
+++ src/screens/Perfil/index.tsx
@@ -1,18 +1,51 @@
-import { View, Text } from "react-native";
-import { ComponentButtonInterface } from "../../components";
-import { ButtonInterface } from "../../components/ButtonInterface";
-import { styles } from "./styles";
-import { TabTypes } from "../../navigations/tab.navigation";
-
-export function Perfil({navigation}:TabTypes) {
-    function handleVoltar(){
-        const tab = navigation.getParent()
-        tab?.goBack()
-    }
+import { View, Text} from "react-native"; 
+import { styles} from "./styles";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { TabTypes } from "../../navigations/tab.navigation"; 
+import * as Notifications from 'expo-notifications';
+import { registerForPushNotificationsAsync } from "../../services/data/Push";
+import { useAuth} from "../../hooks/auth";
+import { useEffect, useState } from "react";
+import { ComponentLoading } from "../../components";
+import React from "react";
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({ 
+        shouldShowAlert: true,
+        shouldPlaySound: false,
+        shouldSetBadge: false,
+    }),
+});
+export function Perfil({ navigation }: TabTypes) {
+    const { user } = useAuth();
+    const [isLoading, setIsLoading] = useState(true);
+    function handleVoltar() {
+        const login = navigation.getParent()
+        login?.goBack()
+    }   
+    useEffect(() => {
+        if (user) {
+            setIsLoading(false);
+        }
+    }, [user]);
+    useEffect(() => {
+        async function fetchToken() { 
+            const token = await registerForPushNotificationsAsync()
+            console.log(token)
+        }
+        fetchToken()
+    }, []);
     return (
-        <View style={styles.container}>
-            <Text>Perfil</Text>
-            <ComponentButtonInterface title="Voltar" type="secondary" onPressI={handleVoltar}/>
-        </View>
+        <>
+            {isLoading ? (
+                <ComponentLoading />
+            ) : (
+                <View style={styles.container}>
+                    <Text>Perfil</Text>
+                    <TouchableOpacity onPress={handleVoltar}>
+                        <Text>Voltar</Text>
+                    </TouchableOpacity>
+                </View>
+            )}
+        </>
     )
 }
